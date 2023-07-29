01131b8adc4ae5860ca0e171b3c44191c1203782
NSN-King <88655788+NSN-King@users.noreply.github.com>
2023-07-29 18:24:25 +0530

    Update views.py

--- studio/views.py
+++ studio/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 
 
 def members(request):
-    return render(request, 'index.html')
+    now = NSN
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
 
 # Create your views here.
